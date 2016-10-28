04334b6e6fcc49568c7b0e9a9f761526bbb364ae
lukaslofterud <lukas.lofterud@gmail.com>
2016-10-28 11:31:09 +0200

    Övning1

--- Slump/Program.cs
+++ Slump/Program.cs
@@ -10,6 +10,15 @@ namespace Slump
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Programmet slumpar fram 10 tal mellan 1-20 och beräknar medelvärdet.");
+            Random randomerare = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int tal = randomerare.Next(20) + 1;
+                Console.Write(tal + "  ");
+            }
+
         }
     }
 }
